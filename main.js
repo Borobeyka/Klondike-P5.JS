4367eaa63aa90df0ca52ca7b3520b1eb6f3a3fe8
Borobeyka <dandr07@yandex.ru>
2022-02-02 21:20:03 +0300

    .

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 var config;
-var card;
+var cards = [];
+var cardIcons = new Map();
 
 function preload() {
     config = loadJSON("config.json");
+    cardIcons.set("clover", loadImage("assets/clover.png"))
+    cardIcons.set("diamond", loadImage("assets/diamond.png"))
+    cardIcons.set("heart", loadImage("assets/heart.png"))
+    cardIcons.set("spade", loadImage("assets/spade.png"))
 }
 
 function setup() {
     createCanvas(config.app.width, config.app.height, P2D);
-    background(config.colors.green);
-    card = new Card(10, 10);
-    card.draw();
-    card = new Card(10, 10);
-    card.draw();
+    background(config.color.green);
+    smooth();
+
+    cards.push(new Card(10, 10, "heart", 7));
+    cards.push(new Card(130, 10, "diamond", 9));
+    cards[0].draw();
+    cards[1].draw();
 }
 
 function draw() {
     //print(card.isClicked());
+}
+
+function mousePressed() {
+    if(mouseButton === LEFT) {
+        cards.forEach(card => {
+            if(card.isClicked())
+                print("Clicked on card id: " + card.getID());
+        });
+    }
 }
--- modules/Card.js
+++ modules/Card.js
@@ -1,19 +1,37 @@
 class Card {
     static TOTAL_CARDS = 0;
 
-    constructor(x, y) {
-        
+    constructor(x, y, suit, nominal) {
         this.id = ++Card.TOTAL_CARDS;
+
         this.x = x;
         this.y = y;
+        this.suit = suit;
+        this.nominal = nominal;
+        this.icon = cardIcons.get(this.suit);
+        this.iconColor = suit == "heart" || suit == "spade" ? config.color.red : config.color.black;
+        print(this.iconColor);
     }
 
     draw() {
+        
+        
         strokeWeight(config.card.stroke);
-        stroke(config.colors.black);
-        fill(config.colors.white);
+        stroke(config.color.black);
+        fill(config.color.white);
         rect(this.x, this.y, config.card.width, config.card.height, config.card.round);
-        print("Draw ID: " + this.id);
+
+        noStroke();
+        fill(this.iconColor);
+        textStyle(BOLD);
+        textAlign(LEFT, TOP);
+        textSize(config.card.properties.textSize);
+        text(this.nominal, this.x + config.card.properties.offset.x, this.y + config.card.properties.offset.y);
+
+
+        tint(this.iconColor);
+        image(this.icon, this.x + config.card.properties.offset.x, this.y + config.card.properties.offset.y + config.card.properties.textSize, config.card.properties.icon.width, config.card.properties.icon.height);
+        
     }
 
     updateCoords(x, y) {
@@ -24,7 +42,7 @@ class Card {
     isClicked() {
         if(mouseX > this.x && mouseX < this.x + config.card.width + config.card.stroke &&
             mouseY > this.y && mouseY < this.y + config.card.height + config.card.stroke &&
-            mouseIsPressed == true && mouseButton === LEFT)
+            mouseIsPressed == true)
             return true;
         return false;
     }
