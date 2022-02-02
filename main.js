9e2448003042c675267a3758ce94ff387ec5d6aa
Borobeyka <dandr07@yandex.ru>
2022-02-02 22:55:56 +0300

    .

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 var config;
 var cards = [];
 var cardIcons = new Map();
+var draggedCard = null;
 
 function preload() {
     config = loadJSON("config.json");
-    cardIcons.set("clover", loadImage("assets/clover.png"))
-    cardIcons.set("diamond", loadImage("assets/diamond.png"))
-    cardIcons.set("heart", loadImage("assets/heart.png"))
-    cardIcons.set("spade", loadImage("assets/spade.png"))
+    cardIcons.set("clover", loadImage("assets/clover.png"));
+    cardIcons.set("diamond", loadImage("assets/diamond.png"));
+    cardIcons.set("heart", loadImage("assets/heart.png"));
+    cardIcons.set("spade", loadImage("assets/spade.png"));
 }
 
 function setup() {
     createCanvas(config.app.width, config.app.height, P2D);
-    background(config.color.green);
     smooth();
 
-    cards.push(new Card(10, 10, "heart", 7));
+    cards.push(new Card(10, 10, "heart", 0));
     cards.push(new Card(130, 10, "diamond", 9));
-    cards[0].draw();
-    cards[1].draw();
+    
 }
 
 function draw() {
-    //print(card.isClicked());
+    background(config.color.green);
+    cards.forEach(card => {
+        card.show();
+    });
+
+    if(draggedCard != null)
+        draggedCard.updateCoords();
 }
 
 function mousePressed() {
     if(mouseButton === LEFT) {
         cards.forEach(card => {
-            if(card.isClicked())
-                print("Clicked on card id: " + card.getID());
+            if(card.isInArea())
+            {
+                draggedCard = card.getObject();
+                draggedCard.mouseOffsetX = abs(mouseX - draggedCard.x);
+                draggedCard.mouseOffsetY = abs(mouseY - draggedCard.y);
+            }
         });
+    }
+}
+
+function mouseReleased() {
+    if(mouseButton === LEFT && draggedCard != null)
+    {
+        draggedCard = null;
     }
 }
--- modules/Card.js
+++ modules/Card.js
@@ -1,45 +1,43 @@
 class Card {
     static TOTAL_CARDS = 0;
 
-    constructor(x, y, suit, nominal) {
+    constructor(x, y, suit, nominal, isVisible = true) {
         this.id = ++Card.TOTAL_CARDS;
 
         this.x = x;
         this.y = y;
         this.suit = suit;
-        this.nominal = nominal;
+        this.nominal = nominal == 0 ? "A" : nominal;
+        this.isVisible = isVisible;
         this.icon = cardIcons.get(this.suit);
         this.iconColor = suit == "heart" || suit == "spade" ? config.color.red : config.color.black;
-        print(this.iconColor);
     }
 
-    draw() {
-        
-        
-        strokeWeight(config.card.stroke);
-        stroke(config.color.black);
-        fill(config.color.white);
-        rect(this.x, this.y, config.card.width, config.card.height, config.card.round);
+    show() {
+        if(this.isVisible) {
+            strokeWeight(config.card.stroke);
+            stroke(config.color.black);
+            fill(config.color.white);
+            rect(this.x, this.y, config.card.width, config.card.height, config.card.round);
 
-        noStroke();
-        fill(this.iconColor);
-        textStyle(BOLD);
-        textAlign(LEFT, TOP);
-        textSize(config.card.properties.textSize);
-        text(this.nominal, this.x + config.card.properties.offset.x, this.y + config.card.properties.offset.y);
+            noStroke();
+            fill(this.iconColor);
+            textStyle(BOLD);
+            textAlign(LEFT, TOP);
+            textSize(config.card.properties.textSize);
+            text(this.nominal, this.x + config.card.properties.offset.x, this.y + config.card.properties.offset.y);
 
-
-        tint(this.iconColor);
-        image(this.icon, this.x + config.card.properties.offset.x, this.y + config.card.properties.offset.y + config.card.properties.textSize, config.card.properties.icon.width, config.card.properties.icon.height);
-        
+            tint(this.iconColor);
+            image(this.icon, this.x + config.card.properties.offset.x, this.y + config.card.properties.offset.y + config.card.properties.textSize, config.card.properties.icon.width, config.card.properties.icon.height);
+        }
     }
 
-    updateCoords(x, y) {
-        this.x += x;
-        this.y += y;
+    updateCoords() {
+        this.x += (mouseX - this.x - this.mouseOffsetX) * 0.35;
+        this.y += (mouseY - this.y - this.mouseOffsetY) * 0.35;
     }
 
-    isClicked() {
+    isInArea() {
         if(mouseX > this.x && mouseX < this.x + config.card.width + config.card.stroke &&
             mouseY > this.y && mouseY < this.y + config.card.height + config.card.stroke &&
             mouseIsPressed == true)
@@ -47,7 +45,11 @@ class Card {
         return false;
     }
 
-    getID() {
-        return this.id;
+    getObject() {
+        return this;
+    }
+
+    changeVisible() {
+        this.isVisible = !this.isVisible;
     }
 }
